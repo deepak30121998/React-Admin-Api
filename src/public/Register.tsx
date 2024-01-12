f7ff64ba58f03b92c33237091eb02c70914dd0a0
deepak30121998 <deepakchoudhary3012@gmail.com>
2024-01-12 16:10:58 +0530

    Register Api Integration Done

--- src/public/Register.tsx
+++ src/public/Register.tsx
@@ -1,50 +1,71 @@
-import React, { Component, SyntheticEvent } from 'react';
+import React, { SyntheticEvent, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Public.css';
+import axios from 'axios';
 
-class Register extends Component {
-    
-    first_name = '';
-    last_name = '';
-    email = '';
-    password = '';
-    confirm_password = '';
+const Register: React.FC = () => {
+    const [state, setState] = useState({
+        first_name: '',
+        last_name: '',
+        email: '',
+        password: '',
+        confirm_password: '',
+    });
 
-    submit = (e: SyntheticEvent) => {
-        e.preventDefault();
+    const navigate = useNavigate();
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { id, value } = e.currentTarget;
         
-        console.log({
-            first_name: this.first_name,
-            last_name: this.last_name,
-            email :this.email,
-            password: this.password,
-            confirm_password: this.confirm_password
-        });
-    }
+        setState((prev) => ({
+            ...prev,
+            [id]: value,
+        }));
+    };
     
-    render() {
-        return (
-            <form className="form-signin" onSubmit={this.submit}>
-            <h1 className="h3 mb-3 font-weight-normal">Please Register</h1>
-            
-            <label htmlFor="firstname" className="sr-only">First Name</label>
-            <input type="text" id="firstname" className="form-control" placeholder="First Name" required onChange={ e => this.first_name = e.target.value } />
-            
-            <label htmlFor="lastname" className="sr-only">Last Name</label>
-            <input type="text" id="lastname" className="form-control" placeholder="Last Name" required onChange={ e => this.last_name = e.target.value } />
-            
-            <label htmlFor="inputEmail" className="sr-only">Email address</label>
-            <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required onChange={ e => this.email = e.target.value } />
 
-            <label htmlFor="inputPassword" className="sr-only">Password</label>
-            <input type="password" id="inputPassword" className="form-control" placeholder="Password" required onChange={ e => this.password = e.target.value } />
-            
-            <label htmlFor="passwordconfirm" className="sr-only">Confirm Password</label>
-            <input type="password" id="passwordconfirm" className="form-control" placeholder="Confirm Password" required onChange={ e => this.confirm_password = e.target.value } />
-            
-            <button className="btn btn-lg btn-primary btn-block" type="submit">Register</button>
-            </form>
-        );
-    }
-}
+    const submit = async (e: SyntheticEvent) => {
+        e.preventDefault();
+
+        try {
+            await axios.post('http://localhost:8000/api/register', {
+                first_name: state.first_name,
+                last_name: state.last_name,
+                email: state.email,
+                password: state.password,
+                password_confirm: state.confirm_password,
+            });
+
+            navigate('/login');
+        } catch (error) {
+            console.error('Registration Error:', error);
+        }
+    };
+
+    return (
+        <form className="form-signin" onSubmit={submit}>
+            <h1 className="h3 mb-3 font-weight-normal">Please Register</h1>
+
+            <label htmlFor="first_name" className="sr-only">First Name</label>
+            <input type="text" id="first_name" className="form-control" placeholder="First Name" required onChange={handleChange} />
+
+            <label htmlFor="last_name" className="sr-only">Last Name</label>
+            <input type="text" id="last_name" className="form-control" placeholder="Last Name" required onChange={handleChange} />
+
+            <label htmlFor="email" className="sr-only">Email address</label>
+            <input type="email" id="email" className="form-control" placeholder="Email address" required onChange={handleChange} />
+
+            <label htmlFor="password" className="sr-only">Password</label>
+            <input type="password" id="password" className="form-control" placeholder="Password" required onChange={handleChange} />
+
+            <label htmlFor="confirm_password" className="sr-only">Confirm Password</label>
+            <input type="password" id="confirm_password" className="form-control" placeholder="Confirm Password" required onChange={handleChange} />
+
+            <button className="btn btn-lg btn-primary btn-block" type="submit">
+                Register
+            </button>
+        </form>
+    );
+};
 
 export default Register;
